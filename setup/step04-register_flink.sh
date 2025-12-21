169a0665dd6718b0c07226d3efcfd1ac39f236a4
Toshiharu-Konuma-sti <t-konuma@sios.com>
2025-12-21 13:53:37 +0900

    added the command to execute flink job

--- setup/step04-register_flink.sh
+++ setup/step04-register_flink.sh
@@ -9,8 +9,12 @@ call_show_start_banner
 SET_DIR=$(call_path_of_setup $CUR_DIR)
 echo "### the dir for setting up  = [$SET_DIR] ##########"
 
-echo "\n### START: Register Flink SQL Job ##########"
+echo "\n### START: Register Flink SQL ##########"
 
 cat $SET_DIR/config/stream_flink.sql | docker exec -i jobmanager ./bin/sql-client.sh
+
+echo "\n### START: Execute Flink Job ##########"
+
+docker exec -it jobmanager ./bin/flink run --python /opt/flink/uppercase_users.py
 
 call_show_finish_banner
