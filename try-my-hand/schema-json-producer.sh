a200db2b72e802a923f364e63d70199dbef062fc
Toshiharu-Konuma-sti <t-konuma@sios.com>
2026-08-13 01:53:56 +0900

    improve scripts

--- try-my-hand/schema-json-producer.sh
+++ try-my-hand/schema-json-producer.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
+set -e
 
 CUR_DIR=$(cd $(dirname $0); pwd)
 . ${CUR_DIR}/variables.sh
-
-if ! docker inspect handson-client > /dev/null 2>&1; then
-  echo "[ERROR] handson-client container is not running." >&2
-  exit 1
-fi
 
 docker cp ${CUR_DIR}/schema-json-producer.py handson-client:/tmp/schema-json-producer.py
 
